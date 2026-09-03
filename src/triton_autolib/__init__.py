86f2c5ee909c12745418b3a3996c8ca488e7d4be
Martico2432 <170757557+Martico2432@users.noreply.github.com>
2026-09-03 12:50:32 +0200

    Update versioning

--- src/triton_autolib/__init__.py
+++ src/triton_autolib/__init__.py
@@ -1,4 +1,10 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("triton-autolib")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+
 from triton_autolib import backward, forward
 from triton_autolib.constants import *
-
-__version__ = "0.1.0"
